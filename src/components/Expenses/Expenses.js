bf839ee46aef27e2b14ead4a4f3900c9c19f881f
Surojit Manna <34436780+isurojit@users.noreply.github.com>
2022-01-12 20:23:30 +0530

    Rendering List Data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,47 +7,22 @@ import { useState } from "react";
 
 const Expenses = (props) => {
   //Another good Option
-  /*const eItems = props.items.map((it) => {
-        return <ExpenseItem title={it.title} date={it.date} price={it.price}/>
-      });
-      
-    return (
-        <div>
-            {eItems}
-        </div>
-        
-    );*/
-    const [selectedOption, setOptionValue] = useState('2020');
-    const optionChanger = (option)=>{
-      setOptionValue(option);
-    }
+  const eItems = props.expenses.map((it) => {
+    return <ExpenseItem title={it.title} date={it.date} amount={it.amount} />;
+  });
+
+  const [selectedOption, setOptionValue] = useState("2020");
+  const optionChanger = (option) => {
+    setOptionValue(option);
+  };
   return (
-    <div>  
+    <div>
       <Card className="expenses">
-      <ExpensesFilter selected={selectedOption} onChangingOption={optionChanger}/>
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
+        <ExpensesFilter
+          selected={selectedOption}
+          onChangingOption={optionChanger}
         />
-
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        />
-
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        />
-
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        />
+        <div>{eItems}</div>
       </Card>
     </div>
   );
